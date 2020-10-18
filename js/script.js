0f59f69171aac92c63383f4d7dc1b105880ef63e
Rovine1999 <rovinewanjala99@gmail.com>
2020-10-18 16:47:01 +0300

    modified script.js, styles.css and index.html

--- js/script.js
+++ js/script.js
@@ -6,66 +6,13 @@ function myFunction() {
  var quant = parseInt(document.getElementById("quantity").value);
  var totalCost = (crust + toppings) * quant;
  alert(`Your order has been received amounting to ${totalCost}`);
- prompt("enter your email address");
- prompt("enter your phone number");
- prompt(`please enter your location.`);
+//  prompt("Enter your email address");
+ prompt("Enter your phone number");
+ prompt("Please enter your location.");
  alert(`Your order will be delivered in a few, please have some ksh.250 together with total amount for delivery.`)
 };
 
-// $(document).ready(function() {
 
-// $("#order").submit(function(event) {
-    
-//     function flavour() {
-//         var flavour = document.getElementById("flavour").value;
-//         return parseInt(Flavour);
-//     }
-//     function size() {
-//         var Size = document.getElementById("#size").value;
-//         return parseInt(Size);
-//     }
-//     function crust() {
-//         var Crust = document.getElementById("#crust").value;
-//         return parseInt(Crust);
-//     }
-//     function toppings() {
-//         var toppings = document.getElementById("#toppings").value;
-//         return parseInt(toppings);
-//     }
-//     function quantity() {
-//         var quantity = document.getElementById("#quantity").value;
-//         return parseInt(quant);
-//     }
-    
-    
-//     function Order(flavour, size, crust, toppings, quantity) {
-//         this.newFlavour = flavour;
-//         this.newSize = size;
-//         this.newCrust = crust;
-//         this.newTopping = toppings;
-//         this.newQuantity = quantity;
-//     }
-    
-//     var userInput = new Order(Flavour(), size(), crust(), topping(), number());
-    
-//     var totalCost =
-//         (userInput.newSize +
-//             userInput.newCrust +
-//             userInput.newToppings +
-//             userInput.newFlavour) *
-//         userInput.newQuantity;
-    
-//     alert("Your charges for Pizza" + totalCost);
-//     prompt("enter your email address");
-//     prompt("enter your phone number");
-//     prompt("enter your location");
-//     alert("Your pizza will be delivered");
-    
-//     $("#text-center").reset();
-//     event.preventDefault();
-//         };
-//     };
-// }
 $(document).ready(function() {
     $(".wanjala").click(function() {
         $("#buffalo").toggle();
