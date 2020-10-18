b754cdc09df8cfc7b7414318e9ab867215b8cc58
Rovine1999 <rovinewanjala99@gmail.com>
2020-10-19 00:30:01 +0300

    modified index.html and script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 function myFunction() {
- var flavour = document.getElementById("flavour").value;
- var size = document.getElementById("size").value;
+ var flavour = parseInt(document.getElementById("flavour").value);
+ var size = (document.getElementById("size").value);
  var crust = parseInt(document.getElementById("crust").value);
+ alert("Your crust is " + crust + "/=")
  var toppings = parseInt(document.getElementById("toppings").value);
+ alert(toppings)
  var quant = parseInt(document.getElementById("quantity").value);
- var totalCost = (crust + toppings) * quant;
+ alert(quant)
+ var totalCost = (size + toppings) * quant;
  alert(`Your order has been received amounting to ${totalCost}`);
-//  prompt("Enter your email address");
  prompt("Enter your phone number");
  prompt("Please enter your location.");
  alert(`Your order will be delivered in a few, please have some ksh.250 together with total amount for delivery.`)
