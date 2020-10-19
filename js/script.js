524153a5992462f552a062b64ad430879856142a
Rovine1999 <rovinewanjala99@gmail.com>
2020-10-19 13:27:33 +0300

    modified script.js, index.html and styles.css

--- js/script.js
+++ js/script.js
@@ -8,12 +8,12 @@ function myFunction() {
  var location = parseInt(document.getElementById("location").value);
  alert("Your location is " + location + "/=")
  var quant = parseInt(document.getElementById("quantity").value);
- alert("Your quantity is " + quant + "pieces")
- var totalCost = (location + ((crust + toppings) * quant));
+ alert("Your quantity is " + quant + " pieces")
+ var totalCost = location + (crust + toppings) * quant;
  alert(`Your order has been received amounting to ${totalCost}`);
  prompt("Enter your phone number");
 //  prompt("Please enter your location.");
- alert(`Your order will be delivered in a few, please have total amount for delivery.`)
+ alert("Your order will be delivered in a few, please have " + totalCost + " amount for delivery.")
 };
 
 
