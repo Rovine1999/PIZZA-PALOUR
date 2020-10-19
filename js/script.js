da2dbd3b812dc8b086a5364bcae29a8979e8a7d1
Rovine1999 <rovinewanjala99@gmail.com>
2020-10-19 16:49:05 +0300

    modified

--- js/script.js
+++ js/script.js
@@ -1,24 +1,23 @@
 function myFunction() {
- var flavour = parseInt(document.getElementById("flavour").value);
- var size = (document.getElementById("size").value);
- var crust = parseInt(document.getElementById("crust").value);
- alert("Your crust is " + crust + "/=")
- var toppings = parseInt(document.getElementById("toppings").value);
- alert("Your topping is " + toppings + "/=")
- var location = parseInt(document.getElementById("location").value);
- alert("Your location is " + location + "/=")
- var quant = parseInt(document.getElementById("quantity").value);
- alert("Your quantity is " + quant + " pieces")
- var totalCost = location + (crust + toppings) * quant;
- alert(`Your order has been received amounting to ${totalCost}`);
- prompt("Enter your phone number");
-//  prompt("Please enter your location.");
- alert("Your order will be delivered in a few, please have " + totalCost + "/=" + " amount for delivery.")
+    var flavour = parseInt(document.getElementById("flavour").value);
+    var size = (document.getElementById("size").value);
+    var crust = parseInt(document.getElementById("crust").value);
+    alert("Your crust is " + crust + "/=")
+    var toppings = parseInt(document.getElementById("toppings").value);
+    alert("Your topping is " + toppings + "/=")
+    var location = parseInt(document.getElementById("location").value);
+    alert("Your location is " + location + "/=")
+    var quant = parseInt(document.getElementById("quantity").value);
+    alert("Your quantity is " + quant + " pieces")
+    var totalCost = location + (crust + toppings) * quant;
+    alert(`Your order has been received amounting to ${totalCost}`);
+    prompt("Enter your phone number");
+    alert("Your order will be delivered in a while, please have " + totalCost + "/=" + " amount for delivery.")
 };
 
 
-$(document).ready(function() {
-    $(".wanjala").click(function() {
+$(document).ready(function () {
+    $(".wanjala").click(function () {
         $("#buffalo").toggle();
         $(".buffalo").toggle();
     });
